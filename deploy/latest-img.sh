501fd0ea2dc39a5ef321f5d0ca7bce5e1613df33
Mantena-Projects <127871554+Mantena-Projects@users.noreply.github.com>
2023-10-21 09:55:59 +0100

    Update latest-img.sh

--- deploy/latest-img.sh
+++ deploy/latest-img.sh
@@ -1,13 +1,10 @@
 taglist="$( docker images $1 | awk '{print $2}' | sed '1d' )"
-echo $taglist
 
 visited=0
 for tag in $taglist
 do
-        echo "This is inspecting tag ********* : " $tag
         createdTime="$( docker inspect -f '{{ .Created }}' $1:$tag )"
         formatedCT=$(date -d "$createdTime" +"%Y%m%d%H%M%S")
-        echo "Formated Time: " $formatedCT
         if [ $visited -eq 0 ]; then
                 greatestValue=$formatedCT
                 visited=1
@@ -20,5 +17,4 @@ do
 done
 
 imageId=$(docker images $1:$latestTag | awk '{print $3}' | sed '1d')
-echo "GREATEST VALUE IS: !!!!!!!!!!!!!!!! : "$greatestValue
-echo "Latest Image is ^^^^^^^^^^^^^^^^^ : "$imageId
+echo $imageId
